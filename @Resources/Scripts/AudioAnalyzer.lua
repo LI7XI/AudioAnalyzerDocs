d873e9c79a044b1585a19e1126cfc46cb046cb14
Liam Olvar <55055075+LI7XI@users.noreply.github.com>
2021-03-18 15:20:08 +0200

    Added Settings skin and few corrections.

--- @Resources/Scripts/AudioAnalyzer.lua
+++ @Resources/Scripts/AudioAnalyzer.lua
@@ -1,6 +1,5 @@
 function Initialize()
   Bands = SELF:GetOption('BandsCount', 1)
-  Wave  = tonumber(SELF:GetOption('Wave', 1))
 
   MeasureBandsFP  = SKIN:MakePathAbsolute(SKIN:ReplaceVariables("#@#Measures\\AudioAnalyzerBands.inc"))
   MeterSpectrumFP = SKIN:MakePathAbsolute(SKIN:ReplaceVariables("#@#Meters\\Spectrum\\AudioAnalyzer.inc"))
@@ -20,7 +19,6 @@ function GenerateMeasureBands(Bands)
     MeasureBands:write("Index=" .. i .. "\n")
     MeasureBands:write("Channel=Auto\n")
     MeasureBands:write("HandlerName=MainFinalOutput\n")
-    -- MeasureBands:write("Group=#GroupName#Measures\n")
     MeasureBands:write("\n")
   end
 
@@ -35,28 +33,16 @@ function GenerateMeterSpectrum(Bands)
   MeterSpectrum:write("X=0\n")
   MeterSpectrum:write("Y=0\n")
 
-
-  if Wave == 1 then 
-    MeterSpectrum:write("Shape=Path WavePath | #Fill# | #Stroke# | Scale 1,-1,0,50\n")
-    MeterSpectrum:write("WavePath=0,#BarWidth# | LineTo 0,(([MeasureBand0]))")
-    for i = 1, (Bands - 1) do
-      MeterSpectrum:write(" | LineTo (#BarXR#*" .. i + 1 .. "),(([MeasureBand".. i .."]))")
-    end
-    MeterSpectrum:write(" | LineTo (#BarXR#*#Bands#),#BarWidth# | ClosePath 1 \n")
-  else
+  MeterSpectrum:write("; ---------------------------------------------------------------------------------------------\n")
+  MeterSpectrum:write("Shape=Rectangle #BarX#,#MaxHeight#,#BarWidth#,(Neg([MeasureBand0])),#Radius# | #Fill# | #Stroke#\n")
+  MeterSpectrum:write("; ---------------------------------------------------------------------------------------------\n")
+  
+  for i = 1, (Bands - 1) do
+    MeterSpectrum:write("Shape"..(i + 1).."=Rectangle #BarX#,#MaxHeight#,#BarWidth#,(Neg([MeasureBand".. i .."])),#Radius# | #Fill# | #Stroke# | Offset (#BarXR#*" .. i .. "),(#BarYR#)\n")
     MeterSpectrum:write("; ---------------------------------------------------------------------------------------------\n")
-    MeterSpectrum:write("Shape=Rectangle #BarX#,#MaxHeight#,#BarWidth#,(Neg([MeasureBand0])),#Radius# | #Fill# | #Stroke#\n")
-    MeterSpectrum:write("; ---------------------------------------------------------------------------------------------\n")
-    
-    for i = 1, (Bands - 1) do
-      MeterSpectrum:write("Shape"..(i + 1).."=Rectangle #BarX#,#MaxHeight#,#BarWidth#,(Neg([MeasureBand".. i .."])),#Radius# | #Fill# | #Stroke# | Offset (#BarXR#*" .. i .. "),(#BarYR#)\n")
-      MeterSpectrum:write("; ---------------------------------------------------------------------------------------------\n")
-    end
-    
   end
-
+  
   MeterSpectrum:write("DynamicVariables=1\n")
-  -- MeterSpectrum:write("Group=#GroupName#Meters\n")
 
   MeterSpectrum:close()
 end
---
+++ @Resources/Scripts/AudioDeviceList.lua
@@ -0,0 +1,24 @@
+--[[ 
+  Special thanks to alatsombath (Github: alatsombath) for creating this method
+  And to marcopixel (Github: marcopixel) for the inspiration
+ ]]
+
+function Initialize()
+  ContextIndex = 1
+
+  for i in string.gmatch(SKIN:ReplaceVariables('[&GetAudioDevices:Resolve(Device List Output)]'), "[^\n]+") do
+    ContextIndex = ContextIndex + 1
+    AudioDeviceInfos = {}
+
+    for j in string.gmatch(i, "[^;]+") do
+      table.insert(AudioDeviceInfos, j) 
+    end
+
+    AudioDeviceID = AudioDeviceInfos[1]
+    AudioDeviceName = AudioDeviceInfos[2]
+
+    SKIN:Bang("!SetOption", "Rainmeter", "ContextTitle" .. ContextIndex, AudioDeviceName)
+    SKIN:Bang("!SetOption", "Rainmeter", "ContextAction" .. ContextIndex, '[!WriteKeyValue Variables AudioDeviceName "' .. AudioDeviceName .. '" "#@#Variables/Common.inc"][!WriteKeyValue Variables AudioDeviceID "ID: ' .. AudioDeviceID .. '" "#@#Variables/Common.inc"][!RefreshGroup [#GroupName]]')
+
+  end
+end
